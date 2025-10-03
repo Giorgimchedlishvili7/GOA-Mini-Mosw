0631ac9fd81aebf88f91f11ea625529c1938f805
Giorgimchedlishvili7 <Tiko2Mariko@gmail.com>
2025-10-02 20:36:48 -0400

    first commit

---
+++ level 001/classwork/python.py
@@ -0,0 +1,18 @@
+#print displays the thing you print example print("giorgi")print is used to display the result on the screen.
+
+username = "giorgi"
+
+print(username)
+
+number1 = 700
+
+number2 = 600
+
+print(number1 + number2)
+
+name = "giorgi"
+
+greeting = "hello"
+
+print(greeting + name)
+
