4624aaaf8c3bf483e5d879a22556ecb668bffd8b
Giorgimchedlishvili7 <Tiko2Mariko@gmail.com>
2025-10-26 16:05:02 -0400

    commit

---
+++ level 009/homework/py.py
@@ -0,0 +1,30 @@
+num1 = (input("Enter your number"))
+
+num2= (input("Enter a number"))
+
+num3= (input("enter last number"))
+
+
+
+n1 = int(num1)
+
+n2 = int(num2)
+
+n3 = int(num3)
+
+print (n1+n2+n3)
+
+
+
+a = "hii"
+
+print (type(a))
+
+b = 5.2
+print (type(b))
+
+c = 7
+print (type(c))
+
+
+#The type() is used to say the data type of a value or variable.
